008bb020076f538e20c00ac3bee37474b54629b2
Robert Christian <860343+robertjchristian@users.noreply.github.com>
2025-02-22 18:46:40 -0500

    bump with explicit export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export { FileContextBuilder } from './FileContextBuilder';
-export * from './types';
-export * from './renderers/Renderer';
-export { ConsoleRenderer } from './renderers/ConsoleRenderer';
-export { JsonRenderer, FileContextJson } from './renderers/JsonRenderer';
+export { FileContextBuilder } from "./FileContextBuilder";
+export * from "./types";
+export * from "./renderers/Renderer";
+export { ConsoleRenderer } from "./renderers/ConsoleRenderer";
+export { JsonRenderer, FileContextJson } from "./renderers/JsonRenderer";
+export { FileCollectorConfig } from "./types";
